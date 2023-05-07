5c62d1503d171da9b9d7ec9d07842e9a7fbeceed
Albina Pasichnyk <albinapasichnyk@Albinas-MacBook-Pro.local>
2023-05-07 06:40:49 +0200

    Task 4 - Guests in rooms

---
+++ Rooms.py
@@ -0,0 +1,5 @@
+# Defining VIP box guests using tuple, since it should be unchangeable
+VIP_box_guests = ('Albina Pasichnyk', 'Bohdan Dovbysh', 'Halyna Far')
+
+# Defining common room with some empty seats, using tuple as amount of seats cannot be changed
+common_room = ('Alisa Forest', 'Bohdan Dovbysh', None, 'Halyna Far', None, 'Oksana Sea', 'Sam Fluffy', None, 'Alf Red')
