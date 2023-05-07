22281e2518e1e8e65f127b29d66518ecef5061cb
Albina Pasichnyk <albinapasichnyk@Albinas-MacBook-Pro.local>
2023-05-07 06:35:02 +0200

    Task 1 - Companies list after overtake

---
+++ Companies.py
@@ -0,0 +1,9 @@
+# Defining 2 Companies Eleks and Toshiba
+Eleks = ['Albina', 'Kostiantyn', 'Bohdan', 'James', 'Sam', 'Peter']
+Toshiba = ['Halyna', 'Oksana', 'Paul', 'Peter', 'Soul', 'Bohdan', 'Katalin']
+
+# Company Toshiba takes over Eleks company
+Toshiba.extend(Eleks)
+
+# Display all people of the Toshiba after it took over Eleks company
+print(Toshiba)
