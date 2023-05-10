899677250729d38ba1fcd6329fdd14181530bcf1
Albina Pasichnyk <albinapasichnyk@Albinas-MacBook-Pro.local>
2023-05-10 22:54:47 +0300

    Task 3.2 - Arithmetic operators

---
+++ HW_3.2_Arithmetic_operators.py
@@ -0,0 +1,26 @@
+# Defining number 2 as a variable
+number = 2
+
+# Multiple 1st way
+multiply_result_1 = number * 2
+print(multiply_result_1)
+
+# Multiple 2nd way using exponentiation
+multiply_result_2 = number ** 2
+print(multiply_result_2)
+
+# Multiply 3rd way using assignment? doubtful
+# number *=2
+# print(number)
+
+# Divide 1st way
+divide_result_1 = number / 2
+print(divide_result_1)
+
+# Divide 2nd way using exponentiation
+divide_result_2 = number // 2
+print(divide_result_2)
+
+# Divide 3nd way using assignment? doubtful
+# number /=2
+# print(number)
